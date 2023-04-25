b0a33634e1a1faf4b68804c01bc45b6b56c52875
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-04-25 20:51:15 -0300

    URI2990 - LEFT JOIN e NOT IN

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2990;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
 
 @SpringBootApplication
@@ -12,13 +17,39 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Autowired
 	private EmpregadoRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2990Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		List<EmpregadoDeptProjection> list1 = repository.serarch1();
+		List<EmpregadoDeptDTO> result1 = list1.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+		System.out.println("Exemplo primeira consulta NOT IN SQL raiz");
+		for (EmpregadoDeptDTO obj : result1) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n\n");
+
+		List<EmpregadoDeptProjection> list2 = repository.serarch2();
+		List<EmpregadoDeptDTO> result2 = list2.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+		System.out.println("Exemplo segunda consulta LEFT JOIN SQL raiz");
+		for (EmpregadoDeptDTO obj : result2) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n\n");
+
+		List<EmpregadoDeptDTO> result3 = repository.serarch3();
+
+		System.out.println("Exemplo primeira consulta JPQL");
+		for (EmpregadoDeptDTO obj : result1) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n\n");
 	}
+
 }
--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,44 @@
 package com.devsuperior.uri2990.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
 import com.devsuperior.uri2990.entities.Empregado;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
 
+	@Query(nativeQuery = true, value = "SELECT empregados.cpf, empregados.enome, departamentos.dnome "
+			+ "FROM empregados "
+			+ "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero "
+			+ "WHERE empregados.cpf NOT IN( "
+			+ "SELECT empregados.cpf FROM empregados "
+			+ "	INNER JOIN trabalha ON trabalha.cpf_emp = empregados.cpf "
+			+ ") "
+			+ "ORDER BY empregados.cpf")
+	List<EmpregadoDeptProjection> serarch1();
+	
+	//Segunda consulta
+	@Query(nativeQuery = true, value = "SELECT empregados.cpf, empregados.enome, departamentos.dnome "
+			+ "FROM empregados "
+			+ "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero "
+			+ "LEFT JOIN trabalha ON trabalha.cpf_emp = empregados.cpf "
+			+ "WHERE trabalha.cpf_emp IS NULL "
+			+ "ORDER BY empregados.cpf")
+	List<EmpregadoDeptProjection> serarch2();
+	
+	//JPQL                                                                                //Navega no atributo          
+	@Query("SELECT new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(obj.cpf, obj.enome, obj.departamento.dnome) "
+			+ "FROM Empregado obj "
+			+ "WHERE obj.cpf NOT IN( "
+			+ "SELECT obj.cpf FROM obj "
+			+ "	INNER JOIN obj.projetosOndeTrabalha " //atributo ManyToMany dentro da classe - identifica os projetos que trabalha
+			+ ") "
+			+ "ORDER BY obj.cpf")
+	List<EmpregadoDeptDTO> serarch3();
 }
+
+
